a703af4fbbb5ba3074058c85f296b3a07301a544
정혜인 <jeonghyein@jeonghyein-ui-MacBookPro.local>
2023-08-09 19:05:32 +0900

    feat: file 업로드 컨트롤러 작성

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { FileService } from './file.service';
 
 @Controller('file')
-export class FileController {}
+export class FileController {
+  constructor(private readonly fileService: FileService) {}
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return this.fileService.uploadFile(file);
+  }
+}
